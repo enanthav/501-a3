235b85b7beb00d2ca1bcf5516bccbc3b117ad5e0
Elizabeth Nanthavong <enanthav@gmail.com>
2017-11-15 22:56:10 -0700

    Added JUnit tests

---
+++ src/TestClass.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.jdom2.Document;
+import org.jdom2.output.XMLOutputter;
+import org.junit.jupiter.api.Test;
+
+import obj.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class TestClass {
+	ArrayObjects arrObj;
+	CollectionObjects collObj;
+	ObjectClass objCls;
+	ObjectRef objRef;
+	PrimitiveArrayClass primArray;
+	PrimitiveClass primClass;
+	
+	@Test
+	public void testCreateArrayObjects() {
+		assertNull(arrObj);
+		arrObj = new ArrayObjects();
+		assertNotNull(arrObj);
+	}
+	
+	@Test
+	public void testCreateCollObj() {
+		assertNull(collObj);
+		collObj = new CollectionObjects();
+		assertNotNull(collObj);
+	}
+	
+	@Test
+	public void testCreateObjectClass() {
+		assertNull(objCls);
+		objCls = new ObjectClass();
+		assertNotNull(objCls);
+	}
+	
+	@Test
+	public void testCreateObjectRef() {
+		assertNull(objRef);
+		objRef = new ObjectRef();
+		assertNotNull(objRef);
+	}
+	
+	@Test
+	public void testCreatePrimArray() {
+		assertNull(primArray);
+		primArray = new PrimitiveArrayClass();
+		assertNotNull(primArray);
+	}
+	@Test
+	public void testCreatePrimClass() {
+		assertNull(primClass);
+		primClass = new PrimitiveClass();
+		assertNotNull(primClass);
+	}
+}
